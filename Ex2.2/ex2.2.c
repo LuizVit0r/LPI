f7c73f455389a1aa4e9e47a710e0757df9860582
LuizVit0r <43145878+LuizVit0r@users.noreply.github.com>
2018-10-03 12:41:03 -0300

    Update ex2.2.c

--- Ex2.2/ex2.2.c
+++ Ex2.2/ex2.2.c
@@ -1,6 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 int main(void){
 	int num1;
 	int num2;
@@ -8,9 +6,18 @@ int main(void){
 	scanf("%d", &num1);
 	printf("digite outro numero: ");
 	scanf("%d", &num2);
-	while(num1 < (num2-1)){
-		num1=num1+1;
-		printf("%d\n", num1);
+	if(num1<num2){
+		while(num1 < (num2-1)){
+			num1=num1+1;
+			printf("%d\n", num1);
+		}
+	}else if(num2<num1){
+		while(num2<(num1-1)){
+			num2=num2+1;
+			printf("%d\n", num2);
+		}
+	}else{
+		printf("0");
 	}
 return 0;
 }
