7ad533fb0a441078701916983387d1ac9dc616d1
LuizVit0r <43145878+LuizVit0r@users.noreply.github.com>
2018-10-04 09:55:48 -0300

    Update Ex6.1.c

--- Ex6.1/Ex6.1.c
+++ Ex6.1/Ex6.1.c
@@ -11,16 +11,19 @@ int preenche(int n,int* vec){
 	}	
 }
 int media(int n,int* vec){
-	int i,soma=0,med=0;
+	int i;
+	float soma=0,med=0;
 	for(i=0;i<n;i+=1){
 		soma+=*(vec+i);
 	}
 	med=(soma/n);
 	printf("===MEDIA===\n");
-	printf("media - %d\n",med);
+	printf("media - %f\n",med);
 }
 int main(void){
-	int n=5,vetor[n];//n=5 só para testes
+	int n,vetor[n];
+	printf("digite a quantidade de numeros: ");
+	scanf("%d",&n);
 	preenche(n,vetor);
 	media(n,vetor);
 	return 0;
