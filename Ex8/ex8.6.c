e7055326d596c9fdc048a9c6e86aad605927ca6f
LuizVit0r <43145878+LuizVit0r@users.noreply.github.com>
2018-12-16 23:22:52 -0200

    Create ex8.6.c

---
+++ Ex8/ex8.6.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct Pessoa {
+    int idade;
+    char nome[25];
+}Pessoa;
+
+int main(void){
+    Pessoa v[100];
+    int n;
+    printf("Digite o numero de pessoas:\n");
+    scanf("%d", &n);
+    FILE* f= fopen("C/Users/Luiz/Documentos/ex8.bin", "wb");
+    fwrite(&n, sizeof(int), 1, f);
+    for (int i=0;i<n;i++){
+        printf("Insira o a idade e o nome da pessoa, respectivamente\n");
+        scanf("%d", &v[i].idade);
+        scanf("%s", v[i].nome);
+        fwrite(&v[i].idade, sizeof(int), 1, f);
+        fwrite(v[i].nome, sizeof(char), (strlen(v[i].nome))+1, f);
+    }
+    fclose(f);
+    return 0;
+}
